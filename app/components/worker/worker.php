56ab3249621e5400dd284d9141cbd51b6e74f9ec
Gyorfi-Balogh <szabolcs.gyorfibalogh@suez.com>
2019-06-04 10:07:45 +0200

    add worker

---
+++ app/components/worker/worker.php
@@ -0,0 +1,32 @@
+<?php
+
+if(isset($_POST)){
+    echo $_POST["quantity"];
+    echo $_POST["quality"];
+    echo $_POST["actualdate"];
+    echo $_POST["nextdate"];
+    echo $_POST["month"];
+    echo $_POST["hufvalue"];
+} else {
+    echo "".
+        ."<md-content layout-padding>
+            <form name='userform' action='worker.php' method='post'>
+                <div layout='row'>
+                    <md-input-container class='md-block' flex>
+                        <md-switch class='md-primary' name='quantity'>
+                            Mennyiségi mutató
+                        </md-switch>
+                    </md-input-container>
+                    <md-input-container class='md-block' flex>
+                        <md-switch class='md-primary' name='quality'>
+                            Minőségi mutató
+                        </md-switch>
+                    </md-input-container>
+                </div>
+            </form>
+        </md-content>".
+    "";
+}
+
+
+?>
